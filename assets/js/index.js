9eb25ae3383d96549165c69e825b7fb95e910855
henriquemsouza <henrique.m_s@hotmail.com>
2019-02-23 20:52:29 -0300

    criado  op get moves

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,12 +2,29 @@ $(document).ready(function () {
   $('.load-games').on('click', function() {
     getPokeList();
   });
+
+  $('.load-moves').on('click', function() {
+    getPokeMoves();
+  });
 })
 
 
 function getPokeList(){  
   $.ajax({
     url: 'https://pokeapi.co/api/v2/pokemon/?limit=10',
+    method: 'GET',
+    success: (resp) => {
+      build(resp)
+    },
+    error: (err) => {
+    
+    }
+  })
+}
+
+function getPokeMoves(){  
+  $.ajax({
+    url: 'https://pokeapi.co/api/v2/move/?limit=10',
     method: 'GET',
     success: (resp) => {
 
@@ -21,14 +38,13 @@ function getPokeList(){
 
 }
 
-
 function build(resp){
   let content = '';
   $.each(resp.results, function (i, val) {
     content += `
     <tr>
       <td>${val.name}</td>
-      <td><a href="${val.url}" class="badge badge-dark">Dark</a></td>
+      <td><button type="button"  value='${val.url}' class="btn btn-dark">Detalhes</button></td>
     </tr>
     `
   });
