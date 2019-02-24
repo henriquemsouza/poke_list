ee48f22695ece55052d866fa1060b17784ab8483
henriquemsouza <henrique.m_s@hotmail.com>
2019-02-23 22:53:08 -0300

    add ness.css

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
 
 
 function getPokeList(amount) {
-  if(!amount){
+  if (!amount) {
     amount = 20
   }
   $.ajax({
@@ -26,15 +26,13 @@ function getPokeList(amount) {
 }
 
 function getPokeMoves(amount) {
-  if(!amount){
+  if (!amount) {
     amount = 20
   }
   $.ajax({
     url: `https://pokeapi.co/api/v2/move/?limit=${amount}`,
     method: 'GET',
     success: (resp) => {
-
-      console.log(resp)
       build(resp)
     },
     error: (err) => {
@@ -45,16 +43,46 @@ function getPokeMoves(amount) {
 }
 
 function build(resp) {
+  $('.main-tbody').html('');
   let content = '';
   $.each(resp.results, function (i, val) {
     content += `
     <tr>
       <td>${val.name}</td>
-      <td><button type="button"  value='${val.url}' class="btn btn-dark">Detalhes</button></td>
+      <td class="more-info-${i}"></td>
+      <td> <input type="button" data-link='${val.url}' data-number='${i}' class='btn btn-dark more-inf' value="Detalhes"></td>
     </tr>
     `
   });
-
   $('.main-tbody').html(content);
-
 }
+
+function moreInfoPoke(e) {
+  let url = $(e).data('link');
+  let number = $(e).data('number');
+
+
+  $.ajax({
+    url: url,
+    method: 'GET',
+    success: (resp) => {
+      console.log(resp)
+      let content = `
+     <div class="nes-container is-dark with-title">
+        <p class="title">ingo/p>
+        <p>Habilidade: ${resp.abilities[0].ability.name}</p>
+      </div>`;
+      $(`more-info-${number}`).html(content);
+    },
+    error: (err) => {
+
+    }
+  })
+}
+
+
+$(function () {
+  $(document).on("click", '.more-inf', function () {
+    moreInfoPoke(this)
+  });
+});
