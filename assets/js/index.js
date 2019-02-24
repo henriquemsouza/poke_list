0a4bb89ccfa2a38e54933811c81c717e675f6c98
henriquemsouza <henrique.m_s@hotmail.com>
2019-02-24 16:29:26 -0300

    ajustando  poke info show

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,7 +17,7 @@ function getPokeList(amount) {
     url: `https://pokeapi.co/api/v2/pokemon/?limit=${amount}`,
     method: 'GET',
     success: (resp) => {
-      build(resp)
+      build(resp, 'poke')
     },
     error: (err) => {
 
@@ -33,7 +33,7 @@ function getPokeMoves(amount) {
     url: `https://pokeapi.co/api/v2/move/?limit=${amount}`,
     method: 'GET',
     success: (resp) => {
-      build(resp)
+      build(resp, 'move')
     },
     error: (err) => {
 
@@ -42,7 +42,7 @@ function getPokeMoves(amount) {
 
 }
 
-function build(resp) {
+function build(resp, type) {
   $('.main-tbody').html('');
   let content = '';
   $.each(resp.results, function (i, val) {
@@ -50,7 +50,7 @@ function build(resp) {
     <tr>
       <td>${val.name}</td>
       <td class="more-info-${i}"></td>
-      <td> <input type="button" data-link='${val.url}' data-number='${i}' class='btn btn-dark more-inf' value="Detalhes"></td>
+      <td> <input type="button" data-link='${val.url}' data-number='${i}' data-type='${type}' class='btn btn-dark more-inf' value="Detalhes"></td>
     </tr>
     `
   });
@@ -67,30 +67,7 @@ function moreInfoPoke(e) {
     method: 'GET',
     success: (resp) => {
       console.log(resp)
-      let abilities = '';
-      $.each(resp.abilities, function (i, val) {
-        abilities += `
-        <p>Habilidade: ${val.ability.name}</p>
-        `
-      });
-      let content = `
-     <div class="nes-container is-dark with-title">
-        <p class="title">${resp.name}</p>
-        <div class="row">
-          <div class="col-md-4">
-          <p>Peso: ${resp.weight}</p>
-          <p>Experiência Base: ${resp.base_experience}</p>
-          ${abilities}
-        </div>
-        <div class="col-md-4">
-          <div class="row">
-            <img src="${resp.sprites.front_default}" alt="front_default">
-            <img src="${resp.sprites.back_default}" alt="back_default">
-            </div>
-          </div>
-        </div>
-      </div>`;
-      $(`.more-info-${number}`).html(content);
+      buildPokeInfo(resp, number);
     },
     error: (err) => {
 
@@ -98,9 +75,76 @@ function moreInfoPoke(e) {
   })
 }
 
+function buildPokeInfo(resp, number) {
+  let game_indices = '';
+  $.each(resp.game_indices, function (i, val) {
+    game_indices += `
+    <p class="game-name">${val.version.name}</p>
+    `
+  });
+
+  let types = [];
+  $.each(resp.types, function (i, val) {
+    types.push(val.type.name)
+  });
+  let moves = '';
+  $.each(resp.moves, function (i, val) {
+    moves += `
+    <p>${val.move.name}</p>
+    `
+  });
+
+
+  let content = `
+<div class="nes-container is-dark with-title">
+   <p class="title">${resp.name}</p>
+   <table class="info-table">
+   <tbody >
+    <tr>
+        <td>
+          <p>Peso: ${resp.weight}</p>
+          <p>Tipo: ${types}</p>
+          <p>Experiência Base: ${resp.base_experience}</p>
+          <p>Aparece nos jogos:<p/>
+             ${game_indices}
+        </td>
+        <td>
+          <div class="row">
+            <img src="${resp.sprites.front_default}" alt="front_default">
+            <img src="${resp.sprites.back_default}" alt="back_default">
+          </div>
+          <div class="row">
+            <img src="${resp.sprites.front_shiny}" alt="front_shiny">
+            <img src="${resp.sprites.back_shiny}" alt="back_shiny">
+          </div>
+        </td>
+        <td>
+          <p>Movimentos aprendidos:<p>
+          ${moves}
+        </td>
+      </tr>
+   </tbody>
+   </table>
+ </div>`;
+  $(`.more-info-${number}`).html(content);
+
+}
+
 
 $(function () {
   $(document).on("click", '.more-inf', function () {
-    moreInfoPoke(this)
+    let type = $(this).data('type');
+    if (type == 'poke') {
+      moreInfoPoke(this)
+    } else {
+      let number = $(this).data('number');
+      let temp_msg = `<div class="message -right">
+                      <div class="nes-balloon from-right">
+                        <p>?????</p>
+                      </div>
+                    </div>`
+      $(`.more-info-${number}`).html(temp_msg);
+    }
+
   });
 });
